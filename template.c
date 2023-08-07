985b457e702cd6c5b661635e46efda98acad1994
FewPz <sharpkung4826@gmail.com>
2023-08-07 15:13:23 +0700

    modify(template): change best template.

--- template.c
+++ template.c
@@ -1,11 +1,5 @@
 #include <stdio.h>
 int main()
 {
-   char name[101];
-   int age;
-
-   scanf("%[^\n]s", &name);
-   scanf(" %d", &age);
-   printf("Hello world, my name is %s, I'm %d years old.", name, age);
    return 0;
 }
